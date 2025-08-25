a109f3afc3ccae0eb09b67151b1c0b8b14d1e7f9
sebaERA <120693970+sebaERA@users.noreply.github.com>
2025-08-25 01:53:36 -0400

    Update build.gradle.kts

--- test/android/app/build.gradle.kts
+++ test/android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         applicationId = "com.example.test"
-        minSdk = 23
+        minSdkVersion flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
